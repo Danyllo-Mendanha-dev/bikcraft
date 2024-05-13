c4fffd036884ab4e08f0c464e1bebcaa05b9723c
Danyllo-Mendanha-dev <danyllomendanha10@gmail.com>
2024-05-13 20:04:05 -0300

    plugin-animar

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ const galeriaContainer = document.querySelector(".bicicleta-imagens");
 function trocarImagem(event) {
   const img = event.currentTarget;
   const media = matchMedia("(min-width: 1000px)").matches;
-  
+
   if (media) {
     galeriaContainer.prepend(img);
   }
@@ -62,3 +62,8 @@ function eventosGaleria(img) {
 }
 
 galeria.forEach(eventosGaleria);
+
+// Plugin Animar
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
